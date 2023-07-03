29b095a97c14429c7ddfbc939233dfd509687f49
Prasad <ppkhose6@gmail.com>
2023-07-03 17:24:45 +0530

    Final commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 
 // MongoDB Setup :
 
-mongoose.connect("mongodb+srv://prasad:"+process.env.PASSWORD+"@cluster0.yijpl91.mongodb.net/blogSiteDB?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://prasad:" + process.env.PASSWORD + "@cluster0.yijpl91.mongodb.net/blogSiteDB?retryWrites=true&w=majority");
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -23,63 +23,70 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // Logic :
 
-let posts = [];
-
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
 
   Blog.find({})
-    .then((result)=>{
-      if(result){
-        res.render("home", {posts: result});
+    .then((result) => {
+      if (result) {
+        res.render("home", { posts: result });
       }
     })
 
 
-  
+
 });
 
-app.get("/about", (req, res)=>{
+app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.get("/contact", (req, res)=>{
+app.get("/contact", (req, res) => {
   res.render("contact");
 });
 
-app.get("/compose", (req, res)=>{
+app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
-app.post("/compose", (req, res)=>{
-  const post = {
-    postTitle : req.body.postTitle,
-    postContent : req.body.postContent
-  };
+app.post("/compose", (req, res) => {
+  const title = req.body.postTitle;
+  const content = req.body.postContent;
+  
+  const newPost = new Blog({
+    title:title,
+    content:content
+  });
 
-  posts.push(post);
-
-  res.redirect("/");
+  newPost.save()
+    .then((result)=>{
+      if(result){
+        res.redirect("/");
+      }
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
 
 });
 
-app.get("/posts/:postName", (req, res)=>{
+app.get("/posts/:postName", (req, res) => {
   let requestedTitle = _.lowerCase(req.params.postName);
   Blog.find({})
-  .then((result) => {
-    result.forEach((document) => {
-      if(document.title.toLocaleLowerCase()===requestedTitle){
-        res.render("post", {postTitle: document.title, postContent: document.content});
-      }
+    .then((result) => {
+      result.forEach((document) => {
+        if (document.title.toLocaleLowerCase() === requestedTitle) {
+          res.render("post", { postTitle: document.title, postContent: document.content });
+        }
+      });
+    })
+    .catch((error) => {
+      console.log("Error retrieving blog data:", error);
     });
-  })
-  .catch((error) => {
-    console.log("Error retrieving blog data:", error);
-  });
 
 
   // posts.forEach((post)=>{
@@ -90,6 +97,6 @@ app.get("/posts/:postName", (req, res)=>{
 
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
